7c85460566550174ec2338ae878d98687dae2220
SouprP <potyrala.igor@gmail.com>
2023-06-17 22:02:39 +0200

    Setup for React

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+
+function App() {
+  return (
+    <div>
+        <footer>
+            <div className="footer_holder">
+                <h1>Spotify Profile</h1>
+                <div>
+                    <form>
+                        <label htmlFor="profile_name">Profile name</label>
+                        <input id="profile_name"></input>
+                    </form>
+                </div>
+            </div>
+        </footer>
+    </div>
+  )
+}
+
+export default App;
